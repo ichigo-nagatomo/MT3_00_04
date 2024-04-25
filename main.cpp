8db2efcafef8fa33213c3a94dd29806db169e61f
ichigo-nagatomo <k023g0008@g.neec.ac.jp>
2024-04-25 23:44:21 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,175 @@
+#include <Novice.h>
+#include <cmath>
+
+struct Vec3 {
+	float x;
+	float y;
+	float z;
+};
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+//単位行列の作成
+Matrix4x4 MakeIdentity4x4() {
+	Matrix4x4 result;
+
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			if (i == j) {
+				result.m[i][j] = 1.0f;
+			} else {
+				result.m[i][j] = 0.0f;
+			}
+		}
+	}
+
+	return result;
+}
+
+
+//行列の積
+Matrix4x4 Multiply(const Matrix4x4 &matrix1 , const Matrix4x4 &matrix2) {
+	Matrix4x4 result;
+
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result.m[i][j] = 0;
+			for (int k = 0; k < 4; ++k) {
+				result.m[i][j] += matrix1.m[i][k] * matrix2.m[k][j];
+			}
+		}
+	}
+
+	return result;
+}
+
+
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result;
+
+	result = MakeIdentity4x4();
+
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[2][1] = std::sin(-radian);
+	result.m[2][2] = std::cos(radian);
+
+	return result;
+}
+
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result;
+
+	result = MakeIdentity4x4();
+
+	result.m[0][0] = std::cos(radian);
+	result.m[0][2] = std::sin(-radian);
+	result.m[2][0] = std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+
+	return result;
+}
+
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result;
+
+	result = MakeIdentity4x4();
+
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[1][0] = std::sin(-radian);
+	result.m[1][1] = std::cos(radian);
+
+	return result;
+}
+
+Matrix4x4 MakeRotateMatrix(const Vec3 &rotate) {
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 matrix = Multiply(rotateXMatrix , Multiply(rotateYMatrix , rotateZMatrix));
+	return matrix;
+}
+
+//
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x , int y , Matrix4x4 matrix) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth , y + row * kRowHeight , "%.02f" , matrix.m[row][column]
+			);
+		}
+	}
+}
+
+
+const char kWindowTitle[] = "LD2B_06_ナガトモイチゴ_MT3_00_04";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	//宣言・初期化
+	Vec3 rotate {0.4f, 1.43f, -0.8f};
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix , Multiply(rotateYMatrix , rotateZMatrix));
+
+	Matrix4x4 rotateM = MakeRotateMatrix(rotate);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		MatrixScreenPrintf(0 , 0 , rotateXMatrix);
+		MatrixScreenPrintf(0 , kRowHeight * 5 , rotateYMatrix);
+		MatrixScreenPrintf(0 , kRowHeight * 5 * 2 , rotateZMatrix);
+		MatrixScreenPrintf(0 , kRowHeight * 5 * 3 , rotateXYZMatrix);
+		MatrixScreenPrintf(0 , kRowHeight * 5 * 4 , rotateM);
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
